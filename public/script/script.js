b22261b13246f4438e996b105ba372f52426c562
Justin Albert <jalbert@carthage.edu>
2020-10-24 23:10:26 -0500

    created navbar

---
+++ public/script/script.js
@@ -0,0 +1,30 @@
+
+function createNavbar(){
+    let newNav=$("<nav>");
+    newNav.addClass("navbar navbar-dark");
+    let newLink=$("<a>");
+    newLink.addClass("navbar-brand brand-font");
+    newLink.attr("href","./index.html");
+    newLink.text("Bear Bottom Diapers");
+    let newImg=$("<img>");
+    newImg.attr("src","../diaper-bear.png");
+    newImg.addClass("d-inline-block-top mr-4");
+    newImg.attr("style","width: 40px; height: 40px");
+    newImg.attr("alt","Diaper Bear");
+
+    newLink.prepend(newImg);
+    
+    let newIcon=$("<i>");
+    newIcon.addClass("fa fa-shopping-cart ml-auto");
+    newIcon.attr("style","font-size: 36px");
+
+    newNav.append(newLink);
+    newNav.append(newIcon);
+    
+
+    $("body").prepend(newNav);
+
+}
+
+
+createNavbar();
